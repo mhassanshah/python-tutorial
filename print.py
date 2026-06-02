12f779503137142e6f302e9b2f23a0e462bba188
mhassanshah <mhassangrt@gmail.com>
2026-06-02 19:50:39 +0500

    Update print.py
    i changed file to add plus, minus , multiply and divide function

--- print.py
+++ print.py
@@ -4,10 +4,9 @@
 
 # This print statement will print the subratction of numbrs 20 and 10
 print(20-10)
-
-
-
-
+print(20+10)
+print(20/10)
+print(20*10)
 
 # this print statement will print "hello world"
 print("hello world")
