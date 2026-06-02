b14ef0b3b39d4882ec45a66e4949d531ccd2c1f0
mhassanshah <mhassangrt@gmail.com>
2026-06-02 22:06:40 +0500

    Update print.py
    some of my new updates of print statement

--- print.py
+++ print.py
@@ -8,5 +8,19 @@ print(20+10)
 print(20/10)
 print(20*10)
 
+
+
+#this print statement will print "yes i am a student"
+print("yes i am a student")
+
+# this print statement will print "i am learning python"
+print("i am learning python")
+
+# this print statement will print "python is a programming language"
+
+
 # this print statement will print "hello world"
 print("hello world")
+
+# this print statement will print "it is awesome"
+print("it is awesome")
