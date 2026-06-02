a7f70e0c54303288d29bf791cb4eef0c95cc6cad
mhassanshah <mhassangrt@gmail.com>
2026-06-02 19:43:18 +0500

    Create print.py
    this is code changes that i made on 1st june  2026

---
+++ print.py
@@ -0,0 +1,13 @@
+# the print function in python prints the output to the screen
+#the  below given function is syntax of print function in  python 0
+
+
+# This print statement will print the subratction of numbrs 20 and 10
+print(20-10)
+
+
+
+
+
+# this print statement will print "hello world"
+print("hello world")
