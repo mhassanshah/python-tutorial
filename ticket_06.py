8110da508531d8764fb3bf3ecd7fa81d2446a993
mhassanshah <mhassangrt@gmail.com>
2026-06-12 15:53:13 +0500

    result of py ticket_06.py
    result of py ticket_06.py

---
+++ ticket_06.py
@@ -0,0 +1,22 @@
+a = 10
+b = 3.5
+print(a + b)
+print(a - b)
+print(a * b)
+print(a / b)
+
+
+print(10 // 3)  # floor division
+print(10 % 3)   # remainder
+print(2 ** 3)   # power
+
+a = 5
+b = 10
+print(a + b)
+
+print(15 % 4)
+
+print(5 ** 2)
+
+print(round(20 / 3, 2))
+
