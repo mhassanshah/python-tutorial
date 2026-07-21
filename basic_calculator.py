1136cb4fe43caf7adbc1869c1fb1b4e788379068
mhassanshah <mhassangrt@gmail.com>
2026-07-21 22:22:54 +0500

    basic_calculator.py
    ticket_8 basic_calculator.py

---
+++ basic_calculator.py
@@ -0,0 +1,47 @@
+number1 = 10
+number2 = 20
+sum = number1 + number2
+print ("sum=",sum)
+
+number1 = "10"
+number2 = "20"
+sum = number1 + number2
+print ("sum=",sum)
+
+number1 = "10.0"
+number2 = "20.0"
+sum = number1 + number2
+print ("sum=",sum)
+
+number1 = 10.0
+number2 = 20.0
+sum = number1 + number2
+print ("sum=",sum)
+
+number1 = 10.0
+number2 = 20.0
+differance = number1 + number2
+print ("differance=",differance)
+
+number1 = "10"
+number2 = "20"
+differance = number1 + number2
+print ("differance=",differance)
+
+
+number1 = 10
+number2 = 20
+division = number1 / number2
+print ("division=",division)
+
+number1 = 10.0
+number2 = 20.0
+division = number1 /number2
+print ("division=",division)
+
+
+number1 = 10
+number2 = 20
+Multiplication	 = number1 * number2
+print ("Multiplication	=",Multiplication	)
+
